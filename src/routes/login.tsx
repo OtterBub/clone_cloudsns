900e4a7bc6f55963d8161192d7ce5fa18a9c4ce9
skpark <gamepsk@gmail.com>
2024-05-14 22:14:42 +0900

    Added signIn signOut

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,112 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { styled } from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+
+// const errors = {
+//     "auth/email-already-in-use": "That email already exists."
+// };
+
+const Title = styled.h1`
+    font-size: 42px;
+`;
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 420px;
+    padding: 50px 0px;
+`;
+const Form = styled.form`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+`;
+const Input = styled.input`
+    padding: 10px 20px;
+    border-radius: 50px;
+    border: none;
+    width: 100%;
+    font-size: 16px;
+    &[type="submit"] {
+        cursor: pointer;
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+`;
+
+const Error = styled.span`
+    padding: 10px 20px;
+    font-weight: 600;
+    color: tomato;
+`;
+
 export default function Login() {
-    return <h1>login!</h1>;
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { target: { name, value } } = e;
+        switch (name) {
+            case "email":
+                setEmail(value);
+                break;
+            case "password":
+                setPassword(value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        if (isLoading || email === "" || password === "") return;
+        try {
+            // change the loading state to true
+            setLoading(true);
+
+            // Create an account
+            const credentials = await signInWithEmailAndPassword(auth, email, password);
+            console.log(credentials.user);
+
+
+            // redirect to the home page
+            navigate("/");
+
+        } catch (e) {
+            // Error
+            // console.log(e);
+            if (e instanceof FirebaseError) {
+                setError(e.message);
+            } else {
+                setError("Unkown Error!");
+            }
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <Wrapper>
+            <Title>Login ☁</Title>
+            <Form onSubmit={onSubmit}>
+                <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+                <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+                <Input type="submit" value={isLoading ? "Loading..." : "Login"} />
+            </Form>
+            {error !== "" ? <Error>{error}</Error> : null}
+        </Wrapper>
+    );
 }
