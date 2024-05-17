2eb636a0129b52e843dbdd1136acee52b55d5277
skpark <gamepsk@gmail.com>
2024-05-18 00:00:24 +0900

    unsubscript when view other page

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import { Unsubscribe, collection, getDocs, limit, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
@@ -13,37 +13,47 @@ export interface ITweet {
     createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+`;
 
 export default function Timeline() {
     const [tweets, setTweet] = useState<ITweet[]>([]);
-    const fetchTweets = async () => {
-        const tweetsQuery = query(
-            collection(db, "tweets"),
-            orderBy("createdAt", "desc")
-        );
-
-        // const snapshot = await getDocs(tweetsQuery);
-        // const tweets = snapshot.docs.map((doc) => {
-        //     console.log(doc.data());
-        //     const { tweet, createdAt, userId, username, photo } = doc.data();
-        //     return {
-        //         tweet, createdAt, userId, username, photo, id: doc.id
-        //     };
-        // });
-        await onSnapshot(tweetsQuery, async (snapshot) => {
-            const tweets = await snapshot.docs.map((doc) => {
-                console.log(doc.data());
-                const { tweet, createdAt, userId, username, photo } = doc.data();
-                return {
-                    tweet, createdAt, userId, username, photo, id: doc.id
-                };
-            });
-            setTweet(tweets);
-        });
-    }
     useEffect(() => {
+        let unsubscribe: Unsubscribe | null = null;
+        const fetchTweets = async () => {
+            const tweetsQuery = query(
+                collection(db, "tweets"),
+                orderBy("createdAt", "desc"),
+                limit(25),
+            );
+
+            // const snapshot = await getDocs(tweetsQuery);
+            // const tweets = snapshot.docs.map((doc) => {
+            //     console.log(doc.data());
+            //     const { tweet, createdAt, userId, username, photo } = doc.data();
+            //     return {
+            //         tweet, createdAt, userId, username, photo, id: doc.id
+            //     };
+            // });
+            unsubscribe = await onSnapshot(tweetsQuery, async (snapshot) => {
+                const tweets = await snapshot.docs.map((doc) => {
+                    console.log(doc.data());
+                    const { tweet, createdAt, userId, username, photo } = doc.data();
+                    return {
+                        tweet, createdAt, userId, username, photo, id: doc.id
+                    };
+                });
+                setTweet(tweets);
+            });
+        }
         fetchTweets();
+        return () => {
+            unsubscribe && unsubscribe();
+
+        }
     }, [])
     return (
         <Wrapper>
