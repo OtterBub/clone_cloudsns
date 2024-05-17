51d157092653972a8290172a6569bc24f3d5993d
skpark <gamepsk@gmail.com>
2024-05-17 19:52:21 +0900

    delete unused contents

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,6 @@
-import { auth } from "../firebase";
 
 export default function Home() {
-    const logOut = () => {
-        auth.signOut();
-    };
-
     return (
-        <h1>
-            This is main Home<br/>
-            <button onClick={logOut}>Logout</button>
-        </h1>
+        <h1> Home </h1>
     );
 }
