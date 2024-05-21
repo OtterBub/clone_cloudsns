00ff9162e3c2343c4ef89cfcd9a1bb359430066b
skpark <gamepsk@gmail.com>
2024-05-22 02:33:49 +0900

    profile page layout

--- src/routes/profiles.tsx
+++ src/routes/profiles.tsx
@@ -1,3 +1,25 @@
+import { styled } from "styled-components";
+import { auth } from "../firebase";
+
+const Wrapper = styled.div``;
+const AvatarUpload = styled.div``;
+const AvatarImg = styled.img``;
+const AvatarInput = styled.input``;
+const Name = styled.div``;
+
 export default function Profile() {
-    return <h1>Profile!</h1>
+    const user = auth.currentUser;
+
+    return (
+        <Wrapper>
+            <AvatarUpload>
+                <AvatarImg />
+            </AvatarUpload>
+            <AvatarInput type="file" accept="image/*" />
+            <Name>
+                {user?.displayName ?? "Anonymous"}
+            </Name>
+        </Wrapper>
+
+    );
 }
